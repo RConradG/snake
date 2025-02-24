d23aa8d332ed6cafd16d869093d695a9b7d8bd70
RConradGe <rcgadduang@gmail.com>
2025-02-24 12:17:01 -0800

    Updated with Task List

--- js/snake.js
+++ js/snake.js
@@ -1,9 +1,67 @@
-/*-------------------------------- Constants --------------------------------*/
+/*-------------- Constants -------------*/
+let FOOD
+ 
+ 
+/*---------- Variables (state) ---------*/
+// Time/Timer
+// Score
+let snake;
+ 
+ 
+/*----- Cached Element References  -----*/
+ 
+// querySelect start game button
+ 
+/*-------------- Functions -------------*/
+ 
+// growSnake:
+// Grow snake once food is eaten
+ 
+// gameOver:
+// Freeze game board, show game over message
+ 
+// showYouLose:
+// Show the message "You Lost!"
+ 
+// showYouWin:
+// Show the meesage "You Win"
+ 
+// showScore:
+// Show the current score
+ 
+// updateScore:
+// When the snake "eats" update score
+ 
+// moveFood:
+// Move food to a random place on the grid
 
-/*-------------------------------- Variables --------------------------------*/
+// render():
+// Will start and restart game
+ 
+/*----------- Event Listeners ----------*/
+// will use the keyboard for snake control, might include option to use arrow keys
+// Listen for certain keyboard keys to be entered
+// I = up
+// J = left
+// K = right
+// N = down
+ 
+// Listen for start button to be pressed (possibly use a keyboard key?)
+ 
+// Listen for pause button to be pressed (possible use keyboard key?)
 
-/*------------------------ Cached Element References ------------------------*/
+// Listen for one keyboard key to start, pause, and restart game?
 
-/*----------------------------- Event Listeners -----------------------------*/
-
-/*-------------------------------- Functions --------------------------------*/
+// • Create Grid
+// • Create food
+// • Create snake
+// • Figure out how to move Snake
+// • Figure out how to end game when snake hits wall
+// • Figure out how to end game when snake eats itself
+// • Figure out how to move snake to food
+// • Figure out how to grow snake after eating
+// • Figure out how to make food disappear when snake eats food
+// • Figure out how to randomly place food on grid
+// • Figure out how to set a timer
+// • Figure out how to end game when timer ends
+// Figure out how to reset time when food is eaten
