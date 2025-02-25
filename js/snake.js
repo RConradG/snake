15ffe65e4491f32245f688b5e5759ed9b6c3840d
RConradGe <rcgadduang@gmail.com>
2025-02-25 11:07:11 -0800

    added food and snake

--- js/snake.js
+++ js/snake.js
@@ -1,30 +1,46 @@
 /*-------------- Constants -------------*/
-const boardRows = 20;
-const boardColumns = 20;
-const tileSize = 30;
-let context;
+const boardRows = 30;
+const boardColumns = 30;
+const tileSize = 35;
 let board;
 /*---------- Variables (state) ---------*/
 // Time/Timer
 // Score
 
-let food;
-let snake;
-let snakeLocation = [{ x: 10, y: 10 }];
+let food = {x: 0, y: 0};
+let snake = {x: 0, y: 0}
+let snakeMovement = {x: 0, y: 0}
 
- 
- /*----- Cached Element References  -----*/
- 
+/*----- Cached Element References  -----*/
+
 // querySelect start game button
 const gameBoard = document.getElementById("board");
+const boardContext = gameBoard.getContext("2d")
 // const cells = document.querySelectorAll(".cell");
+
+/*----------- Event Listeners ----------*/
+const controls = document.addEventListener("keyup", changeDirection);
+
+// will use the keyboard for snake control, might include option to use arrow keys
+// Listen for certain keyboard keys to be entered
+// I = up
+// J = left
+// K = right
+// N = down
+ 
+// Listen for start button to be pressed (possibly use a keyboard key?)
+ 
+// Listen for pause button to be pressed (possible use keyboard key?)
+
+// Listen for one keyboard key to start, pause, and restart game?
+
 
 /*-------------- Functions -------------*/
 render();
 // window.onload = function() {
   
 //   update();
-  
+
 // }
 
 function update() {
@@ -32,27 +48,66 @@ function update() {
 
 function render() {
   createGameBoard();
+  setSnake();
+  setFood();
   // setFood();
   // setSnake();
 };
 
+function moveSnake(controls) {
+  if (controls.code === "ArrowUp") {
+    snakeMovement.x = 0;
+    snakeMovement.y = -1;
+  } else if (controls.code === "ArrowDown") {
+    snakeMovement.x = -1;
+    snakeMovement.y = 0;
+  } else if (controls.code === "ArrowLeft") {
+    snakeMovement.x = -1;
+    snakeMovement.y = 0;
+  } else if (controls.code === "ArrowRight") {
+    snakeMovement.x = 1;
+    snakeMovement.y = 0;
+  }
+}
 
 function createGameBoard () {
   gameBoard.height = tileSize * boardRows;
   gameBoard.width = tileSize * boardColumns
-  context = gameBoard.getContext("2d")
-  context.fillStyle = "black";
-  context.fillRect(0, 0, gameBoard.width, gameBoard.height);
+  boardContext.fillStyle = "black";
+  boardContext.fillRect(0, 0, gameBoard.width, gameBoard.height);
 }
 
-// function setFood() {
-//   food = document.getElementById("cell-38");
-//   // food.setAttribute("class", "food");
-//   food.classList.add("food") // maybe use?
-// }
+function setFood() {
+  // ensures food stays within game board
+  food.x = getRandomLocation().x; 
+  food.y = getRandomLocation().y;
+  boardContext.fillStyle = "yellow";
+  boardContext.fillRect(food.x, food.y, tileSize, tileSize);
+  
+}
+// TODO: make sure setFood location and setSnake location doesn't match
+function setSnake() {
+  snake.x = getRandomLocation().x;
+  snake.y = getRandomLocation().y;
+  boardContext.fillStyle = "green";
+  boardContext.fillRect(snake.x, snake.y, tileSize, tileSize);
+}
 
-// function setSnake() {
-//   snake = cells[3];
+function getRandomLocation() {
+  let location = {x: 0, y: 0}
+  location.x = Math.floor(Math.random() * boardColumns) * tileSize;
+  location.y = Math.floor(Math.random() * boardRows) * tileSize;
+  return location;
+  
+}
+// function setFood() {
+  //   food = document.getElementById("cell-38");
+  //   // food.setAttribute("class", "food");
+  //   food.classList.add("food") // maybe use?
+  // }
+  
+  // function setSnake() {
+    //   snake = cells[3];
 //   snake.setAttribute("class", "snake");
 // }
 // growSnake:
@@ -79,23 +134,9 @@ function createGameBoard () {
 // render():
 // Will start and restart game
  
-/*----------- Event Listeners ----------*/
-// will use the keyboard for snake control, might include option to use arrow keys
-// Listen for certain keyboard keys to be entered
-// I = up
-// J = left
-// K = right
-// N = down
- 
-// Listen for start button to be pressed (possibly use a keyboard key?)
- 
-// Listen for pause button to be pressed (possible use keyboard key?)
 
-// Listen for one keyboard key to start, pause, and restart game?
 
-// • Create Grid
-// • Create food
-// • Create snake
+
 // • Figure out how to move Snake
 // • Figure out how to end game when snake hits wall
 // • Figure out how to end game when snake eats itself
